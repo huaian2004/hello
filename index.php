3acf3bdf0c3540e6abd537aedf6aa4cb469e0499
student <student@test123.com>
2021-09-27 02:55:57 -0400

    added hostname

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-	print "Hello world. Current PHP version is " . PHP_VERSION . "\n";
+	print "Hello world. Current PHP version is " . PHP_VERSION . "<br/>";
+	print "The app is running on host: " . $_ENV["HOSTNAME"] ;
 ?>
